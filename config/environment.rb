bea1f2594c6f64d8e9043a592c0993b6e4ffe5b4
Mike Blake <chikoon@gmail.com>
2014-09-11 18:37:01 +0200

    created a new rails 3.2.19 app named mentions2tasks

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Mentions2tasks::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mentions2tasks::Application.config.secret_token = '21c372fc645e65fdf99d4c89e2ab1204663e9e6972f1714671ccb096ed160904bb0d6e644fd22ab41249b504e71c5f92392ab936818162b56be38bda98149311'
