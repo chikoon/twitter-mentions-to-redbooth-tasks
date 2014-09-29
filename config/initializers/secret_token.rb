14c4f1d9c21094c3be8f02dd1c243d2ba738d92a
Mike Blake <chikoon@gmail.com>
2014-09-29 03:03:26 +0200

    Changed secret token and now allow it to be overwritten by setting ENV['SECRET_TOKEN']

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mentions2tasks::Application.config.secret_token = '21c372fc645e65fdf99d4c89e2ab1204663e9e6972f1714671ccb096ed160904bb0d6e644fd22ab41249b504e71c5f92392ab936818162b56be38bda98149311'
+Mentions2tasks::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'Fu22yWu22Yw454B34rFu22yWu22yH4dN0h41Rfu22Ywu22Yw422NtFu22yW4sH3?Fu22yWu22Yw454B34rFu22yWu22yH4dN0h41Rfu22Ywu22Yw422NtFu22yW4sH3?.*'
+
